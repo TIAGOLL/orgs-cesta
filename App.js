23895c356a17b379d63118213584b37eca2dd416
Tiago Emanuel <tiagopitanga10@hotmail.com>
2023-03-16 16:09:28 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar, SafeAreaView, StyleSheet} from 'react-native';
 import Cesta from './src/telas/Cesta.js'
-//a
+
 export default function App() {
   return (
     <SafeAreaView>
