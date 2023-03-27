16af4b6c33083c5cb4a9ca4fa2ee73819695f231
Tiago Emanuel <tiagopitanga10@hotmail.com>
2023-03-27 14:07:19 -0300

    Estados e ciclo de vida

---
+++ src/mocks/topo.tsx
@@ -0,0 +1,7 @@
+const topo = {
+    boasVindas: 'Olá Tiago',
+    legenda: 'Encontre os melhores produtores',
+}
+
+
+export default topo;
--- src/telas/Home/componentes/Topo.tsx
+++ src/telas/Home/componentes/Topo.tsx
@@ -1,6 +1,56 @@
-import React from "react";
-import { Text } from 'react-native'
+import React from 'react';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
-export default function Topo() {
-    return <Text>Topo</Text>
+import { carregaTopo } from '../../../servicos/carregaDados';
+import Logo from '../../../assets/logo.png';
+
+class Topo extends React.Component {
+    state = {
+        topo: {
+            boasVindas: '',
+            legenda: '',
+        }
+    }
+
+    atualizaTopo() {
+        const retorno = carregaTopo();
+        this.setState({ topo: retorno });
+    }
+
+    componentDidMount() {
+        this.atualizaTopo();
+    }
+
+    render() {
+        return <View style={styles.topo}>
+            <Image source={Logo} style={styles.imagem} />
+            <Text style={styles.boasVindas}>{this.state.topo.boasVindas}</Text>
+            <Text style={styles.legenda}>{this.state.topo.legenda}</Text>
+        </View >
+    }
 }
+
+const styles = StyleSheet.create({
+    topo: {
+        backgroundColor: '#F6F6F6',
+        padding: 16,
+    },
+    imagem: {
+        width: 70,
+        height: 28,
+    },
+    boasVindas: {
+        color: 'black',
+        marginTop: 24,
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold'
+    },
+    legenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        color: 'black',
+    }
+})
+
+export default Topo;
